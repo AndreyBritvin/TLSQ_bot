f28cff095461b21b1652e0ea4a99694fdc1668c0
AndreyBritvin <38183643+AndreyBritvin@users.noreply.github.com>
2018-04-12 10:00:42 +0300

    main
    db перенесена в бота

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 from telegram.ext import CommandHandler, MessageHandler, Filters
 
 import bot
-import db
 import config
+
+
 
 
 def main():
@@ -11,16 +12,13 @@ def main():
     log_file = conf.getVars('LOG_FILE_NAME')
     db_file = conf.getVars('DB_FILE_PATH')
 
-    tg_bot = bot.Bot(token, log_file)
+    tg_bot = bot.Bot(token, log_file,db_file)
 
     start_handler = CommandHandler('start', tg_bot.start)
-    message_handler = MessageHandler(Filters.text, tg_bot.text_handler)
+    message_handler = MessageHandler(Filters.text, tg_bot.object_handler)
     tg_bot.begin_work(start_handler,message_handler)
 
 
-    db_con = db.DB()
-    con = db_con.begin(db_file)
-    cur = con.cursor()
-    con.close()
+
 
 main()
