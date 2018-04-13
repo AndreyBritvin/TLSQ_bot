90f16c6903286d56f023a23b34e0b2e06b5b7d9b
AndreyBritvin <38183643+AndreyBritvin@users.noreply.github.com>
2018-04-13 09:03:16 +0300

    Add files via upload
    Оптимизирована работа с файлами

--- config.py
+++ config.py
@@ -1,13 +1,14 @@
 import json
 
+
 class Read:
-    def __init__(self,place):
+    def __init__(self, place):
         self.place = place
         self._name_variable = None
+        with open(self.place, 'r') as f:
+            self.config = json.loads(f.read())
 
-    def getVars(self,name):
-        with open('config.json','r') as f:
-            config = json.loads(f.read())
-        self._name_variable = config.get(name)
+    def getVars(self, name):
+        self._name_variable = self.config.get(name)
         return self._name_variable
 
