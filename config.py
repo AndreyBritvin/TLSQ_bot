cad8f3d75e274cc986b130e97467f214b3658f76
andrey <andrey.britvin@gmail.com>
2018-04-16 21:31:12 +0300

    дбавлена функция создания словаря

--- config.py
+++ config.py
@@ -5,6 +5,7 @@ class Read:
     def __init__(self, place):
         self.place = place
         self._name_variable = None
+        self._name_dict = None
         with open(self.place, 'r') as f:
             self.config = json.loads(f.read())
 
@@ -12,3 +13,6 @@ class Read:
         self._name_variable = self.config.get(name)
         return self._name_variable
 
+    def getDict(self):
+        self._name_dict = self.config
+        return self._name_dict
