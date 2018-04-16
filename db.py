be6b8ed9cb83aeaa7c26db022c75daf3dbd59726
andrey <andrey.britvin@gmail.com>
2018-04-16 21:34:43 +0300

    добавил фнкцию для создания данных статистики

--- db.py
+++ db.py
@@ -1,6 +1,5 @@
 import sqlite3
-
-
+from time import localtime, strftime
 class DB:
     def create(self, cur, arg):
         cur.execute(arg)
@@ -28,6 +27,55 @@ class DB:
 
         con.commit()
         print("Added into DB")
+
+    def get_stat(self, con, update, mode):
+
+        day = strftime("%Y-%m-%d", localtime())
+        hour = strftime("%Y-%m-%d %H", localtime())
+        # print(hour)
+        cur = con.cursor()
+        # print(update.message)
+        # print(update.message.from_user)
+        cur.execute('select * from Users')
+        users = cur.fetchall()
+        ret = []
+
+        if mode == 0:
+            query = 'select SUM(count_correct ) as "Correct", SUM(count_incorrect) as "Incorrect" from Stat_answers where id_user==?'
+        elif mode == 1:
+            query = 'select SUM(count_correct ) as "Correct", SUM(count_incorrect) as "Incorrect"  from Stat_answers where id_user=? and  time between "' + day + ' 00:00:00" and "' + day + ' 23:59:00"'
+        elif mode == 2:
+            query = 'select SUM(count_correct ) as "Correct", SUM(count_incorrect) as "Incorrect"  from Stat_answers where id_user=? and  time between "' + hour + ':00:00" and "' + hour + ':59:00"'
+
+        # print(query)
+
+        for user in users:
+            # print(user)
+            # ret+=user[2]+" "+user[3]+":"
+            # print(user[0])
+            # cur.execute('select SUM(count_correct ) as "Correct", SUM(count_incorrect) as "Incorrect" from Stat_answers where id_user==?', ( user[0],))
+            cur.execute(query, (user[0],))
+
+            stats = cur.fetchall()
+            # print(stats)
+            if stats[0][0] != None or stats[0][1] != None:
+                for stat in stats:
+                    delta = stat[0] - stat[1]
+                    # ret += str(stat[0]) + " " + str(stat[1]) + " -> "+str(delta)+"\n"
+                    ret.append([user[2], user[3], stat[0], stat[1], delta])
+                ret_sort = sorted(ret, key=lambda x: x[4], reverse=True)
+                # print('Ret', ret_sort)
+                # print(stats)
+                ret_str = ''
+                pos = 1
+                for i in ret_sort:
+                    ret_str += str(pos) + '. ' + i[0] + ' ' + i[1] + ':  ✅:' + str(i[2]) + ' ❌:' + str(
+                        i[3]) + ' 🏆:' + str(i[4]) + '\n'
+                    pos += 1
+        return ret_str
+        # con.commit()
+        # print("Added into DB")
+
 
     def select(self, cur, arg):
         cur.execute(arg)
