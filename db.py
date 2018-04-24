553ce1ded48af36445675df787a1412b04bfd92e
andrey <andrey.britvin@gmail.com>
2018-04-24 20:25:16 +0300

    -закоментированы лишние print`ы

--- db.py
+++ db.py
@@ -5,28 +5,28 @@ class DB:
         cur.execute(arg)
 
     def insert(self, cur, arg):
-        print(arg)
+        # print(arg)
         cur.execute(arg)
 
     def insert_user(self, con, update):
         cur = con.cursor()
-        print(update.message)
-        print(update.message.from_user)
+        # print(update.message)
+        #print(update.message.from_user)
         cur.execute('insert or ignore into Users (tg_userID, tg_first_name, tg_last_name) values (?, ?, ?)', (
         update.message.from_user.id, update.message.from_user.first_name, update.message.from_user.last_name))
         con.commit()
-        print("Added into DB")
+        #print("Added into DB")
 
     def insert_answers(self, con, update, subj, correct, incorrect):
         cur = con.cursor()
-        print(update.message)
-        print(update.message.from_user)
+        # print(update.message)
+        #print(update.message.from_user)
         cur.execute(
             'insert into  Stat_answers(id_user, id_subj, count_correct, count_incorrect, time) values((Select id from Users where tg_userID == ?), (select id from Subj where subj_name == ?), ?, ?, DATETIME("now", "localtime"))',
             (update.message.from_user.id, subj, correct, incorrect))
 
         con.commit()
-        print("Added into DB")
+        #print("Added into DB")
 
     def get_stat(self, con, update, mode):
 
