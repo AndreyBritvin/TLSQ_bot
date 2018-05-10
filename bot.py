716d0b205f82b86a0352f1959f75dcb6224704a9
andrey <andrey.britvin@gmail.com>
2018-05-10 20:40:38 +0300

    -Добавил многопоьзовательский режим - раньше мог отвечать только один пользователь -Текущие ответы хранятся в базе данных -Добавлен ограничитель неправильных ответов

--- bot.py
+++ bot.py
@@ -1,13 +1,8 @@
 from telegram import ReplyKeyboardMarkup
 from telegram.ext import Updater
 import logging
-import time as t
-
-from telegram.ext.dispatcher import run_async
-
 import db
 import random as r
-import copy
 import config
 import Object
 
@@ -18,6 +13,7 @@ class Bot:
             self.updater = Updater(token, request_kwargs={'proxy_url': proxy_url})
         else:
             self.updater = Updater(token)
+
         logging.basicConfig(level=log_level, format='[%(asctime)s] %(levelname)s  %(message)s',
                             filename=filename, filemode='w')
         conf = config.Read(obj_path)
@@ -57,13 +53,15 @@ class Bot:
                 path = self.data[i]["path"]
                 return config.Read(path).getDict()
 
-    def ask_question(self, bot, update):
+    def ask_question(self, bot, update, obj):
 
-        self.quest = Object.get_question(self.obj)
+        self.quest = Object.get_question(obj)
+
+        self.db.insert_questions(self.con, update, self.quest, obj)
 
         self.answers = [self.quest[1]]
         self.answers.append(['Вернуться назад'])
-        bot.sendMessage(chat_id=update.message.chat_id, text=self.obj.question + self.quest[2],
+        bot.sendMessage(chat_id=update.message.chat_id, text=obj.question + self.quest[2],
                         reply_markup=ReplyKeyboardMarkup(self.answers, one_time_keyboard=False, resize_keyboard=True))
 
     def start(self, bot, update):
@@ -79,7 +77,6 @@ class Bot:
         if mes == 'Статистика':
             bot.sendMessage(chat_id=update.message.chat_id, text='Сейчас покажу',
                             reply_markup=ReplyKeyboardMarkup(self.top, one_time_keyboard=False, resize_keyboard=True))
-
 
         elif mes == 'ТОП часа':
             ret = self.db.get_stat(self.con, update, 2)
@@ -108,37 +105,63 @@ class Bot:
                                                              'Удачи!')
 
     def object_handler(self, bot, update):
+
         mes = update.message.text
         self.stat(bot, update)
 
         tmp_obj = Object.get_object(self.objects, mes)
         if tmp_obj:
-            self.obj = tmp_obj
-            self.ask_question(bot, update)
+            obj = tmp_obj
+            self.ask_question(bot, update, obj)
 
+        self.quest_for_check = self.db.select_ques_for_user(self.con, update)
+        # print(self.quest_for_check)
 
         if update.message.text == 'Вернуться назад':
+            self.db.delete_row(self.con, update)
+            #    print('type keyboard', type(self.reply_keyboard))
+            #   print('reply_keyboard', self.reply_keyboard)
             self.question = None
             bot.sendMessage(chat_id=update.message.chat_id, text="Возращаю",
                             reply_markup=ReplyKeyboardMarkup(self.reply_keyboard, one_time_keyboard=True,
                                                              resize_keyboard=True))
+
         try:
             logging.debug(self.quest)
         except AttributeError:
             pass
 
-        if any(mes in s for s in self.quest[1]):
+        self.check_answer(mes, bot, update, self.quest_for_check)
+
+    def check_answer(self, mes, bot, update, quest):
+
+        if any(mes in s for s in quest[1]):
+            obj = Object.get_object(self.objects, self.quest_for_check[2])
             logging.debug("ANSWER IN DICT")
-            if str(mes) == self.quest[0]:
+            if str(mes) == quest[0]:
                 bot.sendMessage(chat_id=update.message.chat_id, text='✅Правильно')
-                self.db.insert_answers(self.con, update, self.obj.name, 1, 0)
-                self.ask_question(bot, update)
+                self.db.insert_answers(self.con, update, obj.name, 1, 0)
+
+                self.ask_question(bot, update, obj)
             else:
-                self.db.insert_answers(self.con, update, self.obj.name, 0, 1)
-                r.shuffle(self.quest[1])
-                bot.sendMessage(chat_id=update.message.chat_id, text='❌Неправильно, попробуй еще',
-                                reply_markup=ReplyKeyboardMarkup(self.answers, one_time_keyboard=False,
-                                                                 resize_keyboard=True))
+
+                inc_answ = self.db.add_incorrect_answer(self.con, update)
+
+                if inc_answ[0][0] == len(quest[1]) - 2:
+                    bot.sendMessage(chat_id=update.message.chat_id,
+                                    text='❌Неправильно\n ‼ На самом деле правильный ответ: ' + quest[0])
+                    self.ask_question(bot, update, obj)
+
+                else:
+                    self.db.insert_answers(self.con, update, obj.name, 0, 1)
+
+                    r.shuffle(quest[1])
+                    ques = []
+                    ques.append(quest[1])
+                    ques.append(['Вернуться назад'])
+                    bot.sendMessage(chat_id=update.message.chat_id, text='❌Неправильно, попробуй еще',
+                                    reply_markup=ReplyKeyboardMarkup(ques, one_time_keyboard=False,
+                                                                     resize_keyboard=True))
 
         logging.info('%s,%s' % (update.message.from_user.first_name, mes))
 
@@ -148,5 +171,4 @@ class Bot:
         self.updater.dispatcher.add_handler(message_handler)
         ##self.updater.dispatcher.add_handler(top)
         self.updater.dispatcher.add_handler(help_handler)
-
-        self.updater.start_polling(poll_interval=1, timeout=30)
+        self.updater.start_polling()
