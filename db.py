a5c23804f596a8ff3bd3d0d739eb72f2675e87c0
andrey <andrey.britvin@gmail.com>
2018-05-10 20:32:56 +0300

    -добавлены фунции: 1) Удаление строки в базе данных 2) Добавлять в базу данных кол-во неправильных ответов 3) Выбирать вопрос в зависимости от его данных в базе данных 4) Запись данных в базу для дальнейшего их использования

--- db.py
+++ db.py
@@ -1,5 +1,7 @@
 import sqlite3
 from time import localtime, strftime
+
+
 class DB:
     def create(self, cur, arg):
         cur.execute(arg)
@@ -11,22 +13,22 @@ class DB:
     def insert_user(self, con, update):
         cur = con.cursor()
         # print(update.message)
-        #print(update.message.from_user)
+        # print(update.message.from_user)
         cur.execute('insert or ignore into Users (tg_userID, tg_first_name, tg_last_name) values (?, ?, ?)', (
-        update.message.from_user.id, update.message.from_user.first_name, update.message.from_user.last_name))
+            update.message.from_user.id, update.message.from_user.first_name, update.message.from_user.last_name))
         con.commit()
-        #print("Added into DB")
+        # print("Added into DB")
 
     def insert_answers(self, con, update, subj, correct, incorrect):
         cur = con.cursor()
         # print(update.message)
-        #print(update.message.from_user)
+        # print(update.message.from_user)
         cur.execute(
             'insert into  Stat_answers(id_user, id_subj, count_correct, count_incorrect, time) values((Select id from Users where tg_userID == ?), (select id from Subj where subj_name == ?), ?, ?, DATETIME("now", "localtime"))',
             (update.message.from_user.id, subj, correct, incorrect))
 
         con.commit()
-        #print("Added into DB")
+        # print("Added into DB")
 
     def get_stat(self, con, update, mode):
 
@@ -76,7 +78,6 @@ class DB:
         # con.commit()
         # print("Added into DB")
 
-
     def select(self, cur, arg):
         cur.execute(arg)
 
@@ -84,4 +85,47 @@ class DB:
         con = sqlite3.connect(path, check_same_thread=False)
         return con
 
+    def get_questions(self, con):
+        cur = con.cursor()
+
+    def insert_questions(self, con, update, question, obj):
+
+        cur = con.cursor()
+        ques_1 = ''
+        for i in range(len(question[1])):
+            ques_1 += question[1][i] + ';'
+
+        cur.execute('insert or replace into Questions(id_user, true_answer, answers, object) values(?, ?, ?, ?)',
+                    (update.message.from_user.id, question[0], ques_1, obj.name))
+        con.commit()
+
+    def select_ques_for_user(self, con, update):
+        cur = con.cursor()
+
+        cur.execute(
+            'select true_answer, answers, object from Questions where id_user ==' + str(update.message.from_user.id))
+        ans = cur.fetchall()
+        try:
+            true_answ = ans[0][0]
+            all_answ = ans[0][1].split(';')
+            obj_user = ans[0][2]
+
+            return true_answ, all_answ, obj_user
+        except:
+            pass
+
+    def delete_row(self, con, update):
+
+        cur = con.cursor()
+        cur.execute('delete from Questions where id_user = ' + str(update.message.from_user.id))
+
+        con.commit()
+
+    def add_incorrect_answer(self, con, update):
+        cur = con.cursor()
+        cur.execute('UPDATE Questions SET counter = counter + 1 WHERE id_user = ' + str(update.message.from_user.id))
+        con.commit()
+        cur.execute('select counter from Questions where id_user =' + str(update.message.from_user.id))
+        counter = cur.fetchall()
+        return counter
 ##select * from stat_answers where time between '2018-04-00 21:00:00'  and '2018-05-00 21:14:00'
